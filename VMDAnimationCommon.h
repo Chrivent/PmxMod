96d96583df898b56cc00c3ce29f514be68972933
Chrivent <yechandubai@gmail.com>
2025-09-09 22:09:40 +0900

    vcxbx

--- VMDAnimationCommon.h
+++ VMDAnimationCommon.h
@@ -12,45 +12,27 @@ namespace saba
 		size_t						startIdx
 	) {
 		if (keys.empty() || keys.size() <= startIdx)
-		{
 			return keys.end();
-		}
 
-		const auto& key0 = keys[startIdx];
-		if (key0.m_time <= t)
-		{
-			if (startIdx + 1 < keys.size())
-			{
-				const auto& key1 = keys[startIdx + 1];
+		const auto &key0 = keys[startIdx];
+		if (key0.m_time <= t) {
+			if (startIdx + 1 < keys.size()) {
+				const auto &key1 = keys[startIdx + 1];
 				if (key1.m_time > t)
-				{
 					return keys.begin() + startIdx + 1;
-				}
-			}
-			else
-			{
+			} else
 				return keys.end();
-			}
-		}
-		else
-		{
-			if (startIdx != 0)
-			{
-				const auto& key1 = keys[startIdx - 1];
-				if (key1.m_time <= t)
-				{
-					return keys.begin() + startIdx;
-				}
-			}
-			else
-			{
-				return keys.begin();
-			}
-		}
+		} else if (startIdx != 0) {
+			const auto &key1 = keys[startIdx - 1];
+			if (key1.m_time <= t)
+				return keys.begin() + startIdx;
+		} else
+			return keys.begin();
 
-		auto bundIt = std::upper_bound(keys.begin(), keys.end(), t, [](int32_t lhs, const KeyType& rhs) {
-			return lhs < rhs.m_time;
-		});
+		auto bundIt = std::upper_bound(keys.begin(), keys.end(), t,
+			[](int32_t lhs, const KeyType &rhs)
+			{ return lhs < rhs.m_time; }
+		);
 		return bundIt;
 	}
 }
