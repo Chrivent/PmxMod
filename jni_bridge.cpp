7b1148ffc3d32250d7e6306aa10981d54ab4eb21
Chrivent <yechandubai@gmail.com>
2025-09-12 20:33:13 +0900

    vbcvbcb

--- jni_bridge.cpp
+++ jni_bridge.cpp
@@ -1,12 +1,14 @@
 #include "jni_bridge.h"
 
 #include "src/MMDModel.h"
+#include "src/VMDCameraAnimation.h"
 #include "base/Path.h"
 
 #include <memory>
 
 struct Core {
     std::unique_ptr<saba::MMDModel> model;
+    std::unique_ptr<saba::VMDCameraAnimation> cameraAnimation;
 };
 
 static Core* from(jlong h) { return reinterpret_cast<Core*>(h); }
@@ -14,12 +16,15 @@ static Core* from(jlong h) { return reinterpret_cast<Core*>(h); }
 JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_create(JNIEnv*, jclass) {
     auto* core = new Core();
     core->model = std::make_unique<saba::MMDModel>();
+    core->cameraAnimation = std::make_unique<saba::VMDCameraAnimation>();
     return reinterpret_cast<jlong>(core);
 }
 
 JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_destroy(JNIEnv*, jclass, jlong h) {
     if (auto* c = from(h)) {
-        c->model.reset(); delete c;
+        c->model.reset();
+        c->cameraAnimation.reset();
+        delete c;
     }
 }
 
