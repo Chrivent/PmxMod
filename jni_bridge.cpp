3bdb2db4b54f44c5197200c7f58d557fc0851da4
Chrivent <yechandubai@gmail.com>
2025-09-13 01:51:57 +0900

    dgsgdf

--- jni_bridge.cpp
+++ jni_bridge.cpp
@@ -24,36 +24,36 @@ static VMDFileCore* VMDFileFrom(jlong h) { return reinterpret_cast<VMDFileCore*>
 
 // 객체 생성 및 해제
 
-JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_new(JNIEnv*, jclass) {
+JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_1new(JNIEnv*, jclass) {
     auto* core = new MMDModelCore();
     return reinterpret_cast<jlong>(core);
 }
 
-JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_delete(JNIEnv*, jclass, jlong h) {
+JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_1delete(JNIEnv*, jclass, jlong h) {
     if (auto* c = MMDModelFrom(h)) {
         c->pmxModel.reset();
         delete c;
     }
 }
 
-JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_new(JNIEnv*, jclass) {
+JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_1new(JNIEnv*, jclass) {
     auto* core = new VMDCameraAnimationCore();
     return reinterpret_cast<jlong>(core);
 }
 
-JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_delete(JNIEnv*, jclass, jlong h) {
+JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_1delete(JNIEnv*, jclass, jlong h) {
     if (auto* c = VMDCameraAnimationFrom(h)) {
         c->vmdCamAnim.reset();
         delete c;
     }
 }
 
-JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_new(JNIEnv*, jclass) {
+JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_1new(JNIEnv*, jclass) {
     auto* core = new VMDFileCore();
     return reinterpret_cast<jlong>(core);
 }
 
-JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_delete(JNIEnv*, jclass, jlong h) {
+JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_1delete(JNIEnv*, jclass, jlong h) {
     if (auto* c = VMDFileFrom(h)) {
         delete c;
     }
@@ -61,15 +61,16 @@ JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_dele
 
 // 함수 명령 전달
 
-JNIEXPORT jboolean JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_Create(JNIEnv *, jclass, jlong camH, jlong vmdH) {
+JNIEXPORT jboolean JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_1Create(JNIEnv *, jclass, jlong camH, jlong vmdH) {
     if (auto* cam = VMDCameraAnimationFrom(camH))
     {
         if (auto* vf = VMDFileFrom(vmdH))
             return cam->vmdCamAnim->Create(vf->vmdFile);
     }
+    return JNI_FALSE;
 }
 
-JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_reset(JNIEnv *, jclass, jlong h) {
+JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_1reset(JNIEnv *, jclass, jlong h) {
     if (auto* VMDCameraAnimation = VMDCameraAnimationFrom(h))
         VMDCameraAnimation->vmdCamAnim.reset();
 }
--- jni_bridge.h
+++ jni_bridge.h
@@ -8,19 +8,19 @@ extern "C" {
     // 패키지/클래스 이름은 자바와 반드시 일치해야 합니다.
     // 예: Java_mod_Native_create → package: "mod", class: "Native"
 
-    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_new(JNIEnv*, jclass);
-    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_delete(JNIEnv*, jclass, jlong);
+    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_1new(JNIEnv*, jclass);
+    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_MMDModelCore_1delete(JNIEnv*, jclass, jlong);
 
-    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_new(JNIEnv*, jclass);
-    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_delete(JNIEnv*, jclass, jlong);
+    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_1new(JNIEnv*, jclass);
+    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDCameraAnimationCore_1delete(JNIEnv*, jclass, jlong);
 
-    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_new(JNIEnv*, jclass);
-    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_delete(JNIEnv*, jclass, jlong);
+    JNIEXPORT jlong JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_1new(JNIEnv*, jclass);
+    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_VMDFileCore_1delete(JNIEnv*, jclass, jlong);
 
     // 함수 명령 전달
 
-    JNIEXPORT jboolean JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_Create(JNIEnv *, jclass, jlong, jlong);
-    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_reset(JNIEnv *, jclass, jlong);
+    JNIEXPORT jboolean JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_1Create(JNIEnv *, jclass, jlong, jlong);
+    JNIEXPORT void JNICALL Java_net_chrivent_pmxstevemod_src_Native_vmdCamAnim_1reset(JNIEnv *, jclass, jlong);
 
 #ifdef __cplusplus
 }
