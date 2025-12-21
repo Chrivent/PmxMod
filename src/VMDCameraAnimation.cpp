b052f71b29c8704ab3637a20cbab1f822eaebaab
Chrivent <yechandubai@gmail.com>
2025-12-21 23:12:04 +0900

    fdgdfg

--- src/VMDCameraAnimation.cpp
+++ src/VMDCameraAnimation.cpp
@@ -36,7 +36,6 @@ VMDCameraController::VMDCameraController()
 void VMDCameraController::Evaluate(const float t) {
 	if (m_keys.empty())
 		return;
-
 	const auto boundIt = FindBoundKey(m_keys, static_cast<int32_t>(t), m_startKeyIndex);
 	if (boundIt == std::end(m_keys)) {
 		const auto &selectKey = m_keys[m_keys.size() - 1];
@@ -64,7 +63,6 @@ void VMDCameraController::Evaluate(const float t) {
 						, m_distanceBezier
 						, m_fovBezier]
 			= *boundIt;
-
 			if ((m_time - key.m_time) > 1) {
 				const auto timeRange = static_cast<float>(m_time - key.m_time);
 				const float time = (t - static_cast<float>(key.m_time)) / timeRange;
@@ -74,14 +72,12 @@ void VMDCameraController::Evaluate(const float t) {
 				const float rotate_x = m_rotateBezier.FindBezierX(time);
 				const float distance_x = m_distanceBezier.FindBezierX(time);
 				const float fov_x = m_fovBezier.FindBezierX(time);
-
 				const float ix_y = m_ixBezier.EvalY(ix_x);
 				const float iy_y = m_iyBezier.EvalY(iy_x);
 				const float iz_y = m_izBezier.EvalY(iz_x);
 				const float rotate_y = m_rotateBezier.EvalY(rotate_x);
 				const float distance_y = m_distanceBezier.EvalY(distance_x);
 				const float fov_y = m_fovBezier.EvalY(fov_x);
-
 				m_camera.m_interest = glm::mix(key.m_interest, m_interest, glm::vec3(ix_y, iy_y, iz_y));
 				m_camera.m_rotate = glm::mix(key.m_rotate, m_rotate, rotate_y);
 				m_camera.m_distance = glm::mix(key.m_distance, m_distance, distance_y);
@@ -96,7 +92,6 @@ void VMDCameraController::Evaluate(const float t) {
 				m_camera.m_distance = key.m_distance;
 				m_camera.m_fov = key.m_fov;
 			}
-
 			m_startKeyIndex = std::distance(m_keys.cbegin(), boundIt);
 		}
 	}
@@ -127,7 +122,6 @@ bool VMDCameraAnimation::Create(const VMDFile & vmd) {
 			key.m_rotate = cam.m_rotate;
 			key.m_distance = cam.m_distance;
 			key.m_fov = glm::radians(static_cast<float>(cam.m_viewAngle));
-
 			const uint8_t *ip = cam.m_interpolation.data();
 			SetVMDBezier(key.m_ixBezier, ip[0], ip[1], ip[2], ip[3]);
 			SetVMDBezier(key.m_iyBezier, ip[4], ip[5], ip[6], ip[7]);
@@ -135,7 +129,6 @@ bool VMDCameraAnimation::Create(const VMDFile & vmd) {
 			SetVMDBezier(key.m_rotateBezier, ip[12], ip[13], ip[14], ip[15]);
 			SetVMDBezier(key.m_distanceBezier, ip[16], ip[17], ip[18], ip[19]);
 			SetVMDBezier(key.m_fovBezier, ip[20], ip[21], ip[22], ip[23]);
-
 			m_cameraController->AddKey(key);
 		}
 		m_cameraController->SortKeys();
@@ -144,13 +137,11 @@ bool VMDCameraAnimation::Create(const VMDFile & vmd) {
 	return true;
 }
 
-void VMDCameraAnimation::Destroy()
-{
+void VMDCameraAnimation::Destroy() {
 	m_cameraController.reset();
 }
 
-void VMDCameraAnimation::Evaluate(const float t)
-{
+void VMDCameraAnimation::Evaluate(const float t) {
 	m_cameraController->Evaluate(t);
 	m_camera = m_cameraController->m_camera;
 }
