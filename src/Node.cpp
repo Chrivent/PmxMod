64b9e587b57d230b81c545f9d510c0ce67c889f9
Chrivent <yechandubai@gmail.com>
2026-01-19 18:43:51 +0900

    vcbcvb

--- src/Node.cpp
+++ src/Node.cpp
@@ -2,37 +2,6 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
-Node::Node()
-	: m_index(0)
-	, m_enableIK(false)
-	, m_parent(nullptr)
-	, m_child(nullptr)
-	, m_next(nullptr)
-	, m_prev(nullptr)
-	, m_translate(0)
-	, m_rotate(1, 0, 0, 0)
-	, m_scale(1)
-	, m_animTranslate(0)
-	, m_animRotate(1, 0, 0, 0)
-	, m_baseAnimTranslate(0)
-	, m_baseAnimRotate(1, 0, 0, 0)
-	, m_ikRotate(1, 0, 0, 0)
-	, m_local(1)
-	, m_global(1)
-	, m_inverseInit(1)
-	, m_initTranslate(0)
-	, m_initRotate(1, 0, 0, 0)
-	, m_initScale(1)
-	, m_deformDepth(-1)
-	, m_isDeformAfterPhysics(false)
-	, m_appendNode(nullptr)
-	, m_isAppendRotate(false)
-	, m_isAppendTranslate(false)
-	, m_isAppendLocal(false)
-	, m_appendWeight(0)
-	, m_ikSolver(nullptr) {
-}
-
 void Node::AddChild(Node* child) {
 	child->m_parent = this;
 	if (!m_child) {
@@ -87,14 +56,14 @@ void Node::UpdateChildTransform() const {
 void Node::UpdateAppendTransform() {
 	if (m_isAppendRotate) {
 		glm::quat appendRotate = !m_isAppendLocal && m_appendNode->m_appendNode
-		? m_appendNode->m_appendRotate : (m_appendNode->m_animRotate * m_appendNode->m_rotate);
+		? m_appendNode->m_appendRotate : m_appendNode->m_animRotate * m_appendNode->m_rotate;
 		if (m_appendNode->m_enableIK)
 			appendRotate = m_appendNode->m_ikRotate * appendRotate;
 		m_appendRotate = glm::slerp(glm::quat(1, 0, 0, 0), appendRotate, m_appendWeight);
 	}
 	if (m_isAppendTranslate) {
 		const glm::vec3 appendTranslate = !m_isAppendLocal && m_appendNode->m_appendNode
-		? m_appendNode->m_appendTranslate : (m_appendNode->m_translate - m_appendNode->m_initTranslate);
+		? m_appendNode->m_appendTranslate : m_appendNode->m_translate - m_appendNode->m_initTranslate;
 		m_appendTranslate = appendTranslate * m_appendWeight;
 	}
 	UpdateLocalTransform();
--- src/Node.h
+++ src/Node.h
@@ -8,9 +8,38 @@
 
 struct IkSolver;
 
-struct Node
-{
-	Node();
+struct Node {
+	uint32_t	m_index = 0;
+	std::string	m_name;
+	bool		m_enableIK = false;
+	Node*		m_parent = nullptr;
+	Node*		m_child = nullptr;
+	Node*		m_next = nullptr;
+	Node*		m_prev = nullptr;
+	glm::vec3	m_translate = glm::vec3(0);
+	glm::quat	m_rotate = glm::quat(1, 0, 0, 0);
+	glm::vec3	m_scale = glm::vec3(1);
+	glm::vec3	m_animTranslate = glm::vec3(0);
+	glm::quat	m_animRotate = glm::quat(1, 0, 0, 0);
+	glm::vec3	m_baseAnimTranslate = glm::vec3(0);
+	glm::quat	m_baseAnimRotate = glm::quat(1, 0, 0, 0);
+	glm::quat	m_ikRotate = glm::quat(1, 0, 0, 0);
+	glm::mat4	m_local = glm::mat4(1);
+	glm::mat4	m_global = glm::mat4(1);
+	glm::mat4	m_inverseInit = glm::mat4(1);
+	glm::vec3	m_initTranslate = glm::vec3(0);
+	glm::quat	m_initRotate = glm::quat(1, 0, 0, 0);
+	glm::vec3	m_initScale = glm::vec3(1);
+	int32_t		m_deformDepth = -1;
+	bool		m_isDeformAfterPhysics = false;
+	Node*		m_appendNode = nullptr;
+	bool		m_isAppendRotate = false;
+	bool		m_isAppendTranslate = false;
+	bool		m_isAppendLocal = false;
+	float		m_appendWeight = 0;
+	glm::vec3	m_appendTranslate = glm::vec3(0);
+	glm::quat	m_appendRotate = glm::quat(1, 0, 0, 0);
+	IkSolver*	m_ikSolver = nullptr;
 
 	void AddChild(Node* child);
 	void BeginUpdateTransform();
@@ -18,36 +47,4 @@ struct Node
 	void UpdateGlobalTransform();
 	void UpdateChildTransform() const;
 	void UpdateAppendTransform();
-
-	uint32_t		m_index;
-	std::string		m_name;
-	bool			m_enableIK;
-	Node*		m_parent;
-	Node*		m_child;
-	Node*		m_next;
-	Node*		m_prev;
-	glm::vec3	m_translate;
-	glm::quat	m_rotate;
-	glm::vec3	m_scale;
-	glm::vec3	m_animTranslate;
-	glm::quat	m_animRotate;
-	glm::vec3	m_baseAnimTranslate;
-	glm::quat	m_baseAnimRotate;
-	glm::quat	m_ikRotate;
-	glm::mat4		m_local;
-	glm::mat4		m_global;
-	glm::mat4		m_inverseInit;
-	glm::vec3	m_initTranslate;
-	glm::quat	m_initRotate;
-	glm::vec3	m_initScale;
-	int32_t		m_deformDepth;
-	bool		m_isDeformAfterPhysics;
-	Node*	m_appendNode;
-	bool		m_isAppendRotate;
-	bool		m_isAppendTranslate;
-	bool		m_isAppendLocal;
-	float		m_appendWeight;
-	glm::vec3	m_appendTranslate;
-	glm::quat	m_appendRotate;
-	IkSolver* m_ikSolver;
 };
