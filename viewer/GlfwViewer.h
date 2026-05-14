a7b81e70314ed0c1a641f1162160314b6f77fcac
Chrivent <yechandubai@gmail.com>
2026-05-14 16:36:51 +0900

    dgdfg

--- viewer/GlfwViewer.h
+++ viewer/GlfwViewer.h
@@ -3,19 +3,8 @@
 #include "Viewer.h"
 
 #include <map>
-#include <string>
 
 class GlfwViewer;
-
-class GlfwShaderHelper {
-public:
-    // GLSL 셰이더 소스를 지정한 타입으로 컴파일한다.
-    static GLuint CompileShader(GLenum shaderType, const std::string& code);
-    // 단일 GLSL 파일에서 버텍스/프래그먼트 분기를 위한 define 줄을 삽입한다.
-    static std::string InjectDefine(const std::string& src, const char* defineLine);
-    // GLSL 파일을 읽어 버텍스/프래그먼트 셰이더 프로그램을 생성한다.
-    static GLuint CreateShader(const std::filesystem::path& file);
-};
 
 class GlfwShader {
     // uniform 이름 목록을 조회해 대응하는 위치 포인터들에 저장한다.
