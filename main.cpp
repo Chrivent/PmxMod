4c4ee9376633c59385f8ea9470f4ea326ed22622
Chrivent <yechandubai@gmail.com>
2026-05-15 05:21:51 +0900

    fdgdfg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 ﻿#include "src/Program/Program.h"
 
+using Chrivent::Program;
+
 int main() {
-    Chrivent::Program program;
-    return program.Run();
+    Program program;
+    if (!program.Run())
+        return 1;
+    return 0;
 }
