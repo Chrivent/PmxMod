cca68929db3546c52ff3bafcea72e4a23de2e588
Chrivent <yechandubai@gmail.com>
2026-05-19 17:10:08 +0900

    fghgfh

--- src/Parser/BinaryReader.cpp
+++ src/Parser/BinaryReader.cpp
@@ -14,42 +14,38 @@ namespace Chrivent {
 		return cur != std::streampos(-1) && cur < end;
 	}
 
-	bool BinaryReader::Read(std::istream& is, void* dst, const std::size_t bytes) {
-		return static_cast<bool>(is.read(static_cast<char*>(dst), static_cast<std::streamsize>(bytes)));
+	void BinaryReader::Read(std::istream& is, void* dst, const std::size_t bytes) {
+		is.read(static_cast<char*>(dst), static_cast<std::streamsize>(bytes));
 	}
 
-	bool BinaryReader::ReadIndex(std::istream& is, int32_t* index, const uint8_t indexSize) {
+	void BinaryReader::ReadIndex(std::istream& is, int32_t* index, const uint8_t indexSize) {
 		switch (indexSize) {
 			case 1: {
 				uint8_t idx;
-				if (!Read(is, &idx))
-					return false;
+				Read(is, &idx);
 				if (idx != 0xFF)
 					*index = static_cast<int32_t>(idx);
 				else
 					*index = -1;
+				break;
 			}
-				return true;
 			case 2: {
 				uint16_t idx;
-				if (!Read(is, &idx))
-					return false;
+				Read(is, &idx);
 				if (idx != 0xFFFF)
 					*index = static_cast<int32_t>(idx);
 				else
 					*index = -1;
+				break;
 			}
-				return true;
 			case 4: {
 				uint32_t idx;
-				if (!Read(is, &idx))
-					return false;
+				Read(is, &idx);
 				*index = static_cast<int32_t>(idx);
+				break;
 			}
-				return true;
 			default:
-				is.setstate(std::ios::failbit);
-				return false;
+				break;
 		}
 	}
 }
--- src/Parser/BinaryReader.h
+++ src/Parser/BinaryReader.h
@@ -10,13 +10,13 @@ namespace Chrivent {
 		// 저장해 둔 끝 위치 이전에 읽을 데이터가 남아 있는지 확인한다.
 		static bool HasMore(std::istream& is, const std::streampos& end);
 		// 지정한 바이트 수만큼 바이너리 스트림에서 읽는다.
-		static bool Read(std::istream& is, void* dst, std::size_t bytes);
+		static void Read(std::istream& is, void* dst, std::size_t bytes);
 		// PMX 헤더의 인덱스 크기 규칙에 맞춰 가변 크기 인덱스를 읽는다.
-		static bool ReadIndex(std::istream& is, int32_t* index, uint8_t indexSize);
+		static void ReadIndex(std::istream& is, int32_t* index, uint8_t indexSize);
 		// POD 값을 바이너리 스트림에서 읽는다.
 		template <typename T>
-		static bool Read(std::istream& is, T* dst) {
-			return Read(is, dst, sizeof(T));
+		static void Read(std::istream& is, T* dst) {
+			Read(is, dst, sizeof(T));
 		}
 	};
 }
