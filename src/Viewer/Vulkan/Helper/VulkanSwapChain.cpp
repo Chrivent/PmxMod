363e0a96d183a8c9bf54eb15431253d5014f46f6
Chrivent <yechandubai@gmail.com>
2026-05-25 00:37:27 +0900

    sdfsdf

--- src/Viewer/Vulkan/Helper/VulkanSwapChain.cpp
+++ src/Viewer/Vulkan/Helper/VulkanSwapChain.cpp
@@ -107,7 +107,11 @@ namespace Chrivent {
 
 	VkSurfaceFormatKHR VulkanSwapChain::ChooseSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats) {
 		for (const auto& format : formats) {
-			if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+			if (format.format == VK_FORMAT_B8G8R8A8_UNORM && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+				return format;
+		}
+		for (const auto& format : formats) {
+			if (format.format == VK_FORMAT_R8G8B8A8_UNORM && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
 				return format;
 		}
 		return formats.front();
