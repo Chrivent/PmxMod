6302e7d6260eaec3f16e680f1af0ee7490ed1345
Chrivent <yechandubai@gmail.com>
2026-06-17 21:56:06 +0900

    asdf

---
+++ src/Viewer/Glsl/Vulkan/Helper/VulkanMemory.cpp
@@ -0,0 +1,17 @@
+﻿#include "VulkanMemory.h"
+
+namespace Chrivent {
+    bool VulkanMemory::FindMemoryType(const VulkanDeviceInfo& deviceInfo, const uint32_t typeFilter,
+        const VkMemoryPropertyFlags properties, uint32_t& memoryType) {
+        VkPhysicalDeviceMemoryProperties memoryProperties{};
+        vkGetPhysicalDeviceMemoryProperties(deviceInfo.physicalDevice, &memoryProperties);
+        for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++) {
+            if ((typeFilter & 1 << i) != 0 &&
+                (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+                memoryType = i;
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/Viewer/Glsl/Vulkan/Helper/VulkanMemory.h
+++ src/Viewer/Glsl/Vulkan/Helper/VulkanMemory.h
@@ -8,19 +8,8 @@ namespace Chrivent {
 		// 물리 디바이스 메모리 중 요청한 속성을 만족하는 memory type index를 찾는다.
 		static bool FindMemoryType(
 			const VulkanDeviceInfo& deviceInfo,
-			const uint32_t typeFilter,
-			const VkMemoryPropertyFlags properties,
-			uint32_t& memoryType) {
-			VkPhysicalDeviceMemoryProperties memoryProperties{};
-			vkGetPhysicalDeviceMemoryProperties(deviceInfo.physicalDevice, &memoryProperties);
-			for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++) {
-				if ((typeFilter & 1 << i) != 0 &&
-					(memoryProperties.memoryTypes[i].propertyFlags & properties) == properties) {
-					memoryType = i;
-					return true;
-				}
-			}
-			return false;
-		}
+			uint32_t typeFilter,
+			VkMemoryPropertyFlags properties,
+			uint32_t& memoryType);
 	};
 }
